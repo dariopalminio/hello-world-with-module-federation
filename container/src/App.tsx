806f99c356eb1e15e7abc8e1cd22d8f31e117570
Daro <dario.palminio@gmail.com>
2021-07-17 00:20:37 -0400

    add container

---
+++ container/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import AppOne from 'app1/AppOne';
+
+
+function App() {
+  return (
+    <div >
+      <h1>Micro Frontends with Module Federation, React and Typescript</h1>
+        Hello world from Container App.
+
+        <AppOne />
+
+    </div>
+  );
+}
+
+export default App;
