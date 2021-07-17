a87ea66300379eb93d32b29ec9745577c9545808
Daro <dario.palminio@gmail.com>
2021-07-17 00:18:52 -0400

    add app1

---
+++ app1/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppOne from './components/AppOne';
+
+
+function App() {
+  return (
+    <div >
+    <AppOne />
+    </div>
+  );
+}
+
+export default App;
---
+++ app1/src/components/AppOne.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const AppOne = () => {
+  
+
+  return (
+    <div>
+      Hello World from Micro App One!
+    </div>
+  );
+};
+
+export default AppOne;
